804b62e2d80c23538b78e4a7371bf0ded8a63eb2
Andrew Fleming <z5164462@unsw.edu.au>
2020-10-18 14:48:28 +1100

    Created classes

---
+++ Client.java
@@ -0,0 +1,30 @@
+
+
+public class Client  {
+    
+
+    public static void main (String[] args) {
+        if(args.length != 2){
+            System.err.println("Usage: java Client server_IP server_port");
+            System.exit(1);
+        }
+
+        Boolean alive = true;
+
+        while(alive){
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+}
---
+++ Server.java
@@ -0,0 +1,17 @@
+
+
+public class Server implements Runnable {
+
+
+    public static void main(String[] args){
+
+        
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
