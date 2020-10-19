7f74d47b11f4c9c8d3bd7900bf73ee98824af7f2
Andrew Fleming <z5164462@unsw.edu.au>
2020-10-19 13:26:37 +1100

    Creating Terminal Prompt enum

--- TerminalText.java
+++ TerminalText.java
@@ -1,28 +1,39 @@
+import java.util.Hashtable;
+
 public enum TerminalText {
     
-    SRVR_WAIT,
-    USNM_PROMPT,
-    PSWD_PROMPT,
-    CLIENT_CNCT,
-    NEW_USER,
-    LOGIN_SUCC,
-    PSWD_FAIL,
-    USER_CMD,
-    THREAD_CRT,
-    THREAD_MSG,
-    THREAD_RDT,
-    THREAD_EDT,
-    BAD_THREADNAME,
-    FILE_UPD,
-    FILE_DWN,
-    BAD_FILE,
-    CANT_RMV,
-    SRVR_SHT,
-    USER_EXIT,
+    // SRVR_WAIT("Waiting For clients"),
+    // USNM_PROMPT("Enter username: "),
+    // PSWD_PROMPT("Enter password: "),
+    // CLIENT_CNCT("Client connected"),
+    // WELCOME("Welcome to the forum"),
+    // NEW_USER("New User"),
+    // LOGIN_SUCC("* Successful Login"),
+    // PSWD_FAIL("* Incorrect * Password"),
+    // USER_CMD("Issued command"),
+    // THREAD_CRT("Thread * created"),
+    // THREAD_MSG("Message posted to * thread"),
+    // THREAD_RDT("Thread * read"),
+    // THREAD_EDT("Message has been edited"),
+    // BAD_EDT(),
+    // BAD_THREADNAME,
+    // FILE_UPD,
+    // FILE_DWN,
+    // BAD_FILE,
+    // CANT_RMV,
+    // SRVR_SHT,
+    // USER_EXIT;
 
 
+    // private final String text;
 
 
+    // private TerminalText(String text) {
+    //     this.text = text;
+    // }
+
+
+    // public getText()
 
 
 
