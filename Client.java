fbb6f28afa72088ced135be0411595d62377f5ee
Andrew Fleming <z5164462@unsw.edu.au>
2020-10-31 21:44:00 +1100

    Some progress. Attempted to used datastream

--- Client.java
+++ Client.java
@@ -6,6 +6,8 @@ import java.io.*;
 
 public class Client  {
     
+    public static byte[] input = new byte[512];
+    public static byte[] output = new byte[512];
 
     public static void main (String[] args) throws NumberFormatException, UnknownHostException, IOException{
         if(args.length != 2){ 
@@ -16,19 +18,23 @@ public class Client  {
         Boolean alive = true;
         
         Socket clientSocket = new Socket(args[0], Integer.parseInt(args[1]));
+        BufferedReader in_from_user = new BufferedReader(new InputStreamReader(System.in));
+        DataInputStream in_from_server = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream out_to_server = new DataOutputStream(clientSocket.getOutputStream());
 
         while(alive){
             
             
-            BufferedReader in_from_user = new BufferedReader(new InputStreamReader(System.in));
-            BufferedReader in_from_server = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            DataOutputStream out_to_server = new DataOutputStream(clientSocket.getOutputStream());
-            
-            System.out.print("#" +in_from_server.readLine());
+            System.out.println("Start");
             
             
-            out_to_server.writeBytes(in_from_user.readLine()+"\n");
+            System.out.print(input);
+            
+            in_from_user.
+            write(in_from_user.readLine().getBytes());
 
+
+            System.out.println("End");
 
 
         }
--- Server.java
+++ Server.java
@@ -1,18 +1,32 @@
 import java.io.*;
 import java.net.*;
-import java.nio.file.*;
+
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
-import msgs.*;
 
-public class Server implements Runnable {
 
-    private static Boolean alive = true;
+
+
+public class Server extends Thread {
+
+    private static Boolean shutdown = false;
     private static Hashtable<String,String> user_pass;
+    private static List<Server> server_list;
+
+    private boolean alive;
+    private Socket socket;
+    private DataInputStream in_from_client;
+    private DataOutputStream out_to_client;
+
+    public Server(Socket s) throws IOException {
+        this.socket = s;
+        in_from_client = new DataInputStream(socket.getInputStream());
+        out_to_client = new DataOutputStream(socket.getOutputStream());
+    }
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         if(args.length != 2){
@@ -20,63 +34,92 @@ public class Server implements Runnable {
             System.exit(1);
         }
         user_pass = new Hashtable<String,String>();
-        
-
-        ServerSocket login_socket = new ServerSocket(Integer.parseInt(args[0]));
-
-
-
-        //Load Username and Passwords into Hashtable
-        List<String> credentials = readInText("./credentials.txt");
-        String[] retval;
-        Iterator<String> it = credentials.iterator();
-        while(it.hasNext()){
-            retval = it.next().split(" ");
-            user_pass.put(retval[0], retval[1]);
-        }
-        BufferedReader in_from_client;
-        DataOutputStream out_to_client;
-
-
-        
-        while(alive){
+        server_list = new ArrayList<Server>();
+        ServerSocket sock = new ServerSocket(Integer.parseInt(args[0]));
+        while(!shutdown){
             System.out.println(TerminalText.SRVR_WAIT.getText());
-            Socket connected = login_socket.accept();
-            System.out.println(TerminalText.CLIENT_CNCT.getText());
-            in_from_client = new BufferedReader(new InputStreamReader(connected.getInputStream()));
-            out_to_client = new DataOutputStream(connected.getOutputStream());
-            
-
-            //Login
-            out_to_client.writeBytes(TerminalText.USNM_PROMPT.getText()+"\n");
-            String username = in_from_client.readLine();
-            if(user_pass.containsKey(username)){
-                out_to_client.writeBytes(TerminalText.PSWD_PROMPT.getText()+"\n");
-                if(user_pass.get(username).equals(in_from_client.readLine())){
-                    out_to_client.writeBytes(TerminalText.WELCOME.getText()+"\n");
-                    System.out.println(TerminalText.LOGIN_SUCC.getText(username));
-                }
-            }else{
-                out_to_client.writeBytes(TerminalText.NEW_USER.getText()+"\n");
-            }
-            
-
-
-
-
-
+            Socket connected = sock.accept();
+            Server svr = new Server(connected);
+            server_list.add(svr);
+            svr.start();
         }
-
-
-
-
+        for(Server s: server_list){
+            s.interrupt();
+        }
+        
     }
 
     @Override
     public void run() {
         // TODO Auto-generated method stub
+        try {
+            List<String> credentials = readInText("./credentials.txt");
+            String[] retval;
+            Iterator<String> it = credentials.iterator();
+            while(it.hasNext()){
+                retval = it.next().split(" ");
+                user_pass.put(retval[0], retval[1]);
+            }
+
+            System.out.println(TerminalText.CLIENT_CNCT.getText());
+
+            out_to_client.writeBytes("Test1\n");
+            out_to_client.writeBytes("Test2\n");
+
+
+            String username = "error";
+            boolean logged_in = false;
+            while(!logged_in){
+                out_to_client.writeBytes(TerminalText.USNM_PROMPT.getText());
+                username = in_from_client.readUTF();
+                if(user_pass.containsKey(username)){
+                    System.out.println("VALID USERNAME");
+                    out_to_client.writeBytes(TerminalText.PSWD_PROMPT.getText());
+                    if(user_pass.get(username).equals(in_from_client.readUTF())){
+                        logged_in = true;
+                    }else{
+                        out_to_client.writeBytes(TerminalText.PSWD_FAIL.getText());
+                        System.out.println(TerminalText.PSWD_FAIL.getText());
+                    }
+                }else{
+                    System.out.println(TerminalText.NEW_USER.getText());
+                    out_to_client.writeBytes(TerminalText.NEW_PSWD.getText(username));
+                    credentials.add(username + " " + in_from_client.readUTF());
+                    writeOutString("./credentials.txt", credentials);
+                    logged_in = true;
+
+                }
+            }                        
+            System.out.println(TerminalText.LOGIN_SUCC.getText(username));
+            out_to_client.writeBytes(TerminalText.WELCOME.getText());
+            in_from_client.readUTF();
+
+
+            String cmd = "";
+            while(!cmd.equals("quit")){
+                out_to_client.writeBytes(TerminalText.CMD_PROMPT.getText());
+                cmd = in_from_client.readUTF();
+
+            }
+            out_to_client.writeBytes("DONE");
+            System.out.println("DONE");
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
+
+
+
 
     }
+
+
     
 // from https://techblogstation.com/java/read-text-file-in-java/
 
@@ -91,7 +134,18 @@ public class Server implements Runnable {
         return text;
     }
 
+    private static void writeOutString(String pathname, List<String> text) throws IOException {
+        File file = new File(pathname);
+        FileWriter fw = new FileWriter(file);
     
+        Iterator<String> it = text.iterator();
+        while(it.hasNext()){
+            fw.write(it.next() + "\n");
+        }
+        fw.flush();
+        fw.close();
+        
+    }
 
 
 }
